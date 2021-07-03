cb569c3c7a5260ad45392905e58aca7d9fdaa864
william-harly <william.harly.wh@gmail.com>
2021-07-03 15:15:02 +0700

    integrate endpoint

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
+import axios from 'axios'
 import '../styles/globals.css'
+
+// axios.defaults.baseURL = process.env.NEXT_PUBLIC_HOST_API
+axios.defaults.baseURL = "http://ec2-18-181-167-59.ap-northeast-1.compute.amazonaws.com:8080"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
